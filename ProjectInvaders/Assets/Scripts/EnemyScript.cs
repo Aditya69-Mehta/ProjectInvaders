53caf9b2fc4dc4a618b4e7be61668139ae588878
Aditya69-Mehta <ksg.aditya.mehta@gmail.com>
2024-07-27 02:36:47 +0530

    EnemyWaveTimeLine1
    Only 2 Enemy Wave Animations Have Been Created

--- ProjectInvaders/Assets/Scripts/EnemyScript.cs
+++ ProjectInvaders/Assets/Scripts/EnemyScript.cs
@@ -10,14 +10,16 @@ public class EnemyScript : MonoBehaviour
     [SerializeField] ParticleSystem enemyExplosionVFX;
     [SerializeField] ParticleSystem enemyHitVFX;
 
-    [SerializeField] Transform parent;
     [SerializeField] int enemyHitPoint = 1;
     [SerializeField] int enemyHealthPoints = 1;
 
+    GameObject spawnParent;
     ScoreBoard scoreBoard;
 
     void Start(){
         scoreBoard = FindAnyObjectByType<ScoreBoard>();
+        spawnParent = GameObject.FindWithTag("SpawnAtRunTime");
+
         gameObject.AddComponent<Rigidbody>().useGravity=false;
         enemyHealthPoints *= 2;
     }
@@ -34,14 +36,14 @@ public class EnemyScript : MonoBehaviour
     void HitProcess()
     {
         ParticleSystem hitVFX = Instantiate(enemyHitVFX, transform.position, Quaternion.identity);
-        hitVFX.transform.parent = parent;
+        hitVFX.transform.parent = spawnParent.transform;
         enemyHealthPoints--;
     }
 
     void EnemySpawnAndDestroy()
     {
         ParticleSystem vfx = Instantiate(enemyExplosionVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
+        vfx.transform.parent = spawnParent.transform;
         Destroy(this.gameObject);
     }
 
